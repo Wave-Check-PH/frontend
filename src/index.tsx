727d6f6e671fd515473867c551becc94b685974d
Vinny <wam@dynamicdeals.ph>
2024-03-30 07:38:49 +0800

    Matomo debug

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import "./matomo.jsx";
+import "./analytics.jsx";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
