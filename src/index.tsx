ae9f575f8207d9077f69d10d5675655ee8d4c6f7
Vinny <wam@dynamicdeals.ph>
2024-03-30 06:33:33 +0800

    Debugging matoma script

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import "./matomo.js";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-
     <BrowserRouter>
       <App />
     </BrowserRouter>
